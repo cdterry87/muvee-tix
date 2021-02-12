7fedfbeff1f461b5479dd497135772277d2d4590
Chase Terry <chase.terry87@gmail.com>
2021-02-11 21:38:45 -0600

    Wrote a test for the home page

---
+++ src/mocks/movies.js
@@ -0,0 +1,148 @@
+const movies = [
+  {
+    adult: false,
+    backdrop_path: '/srYya1ZlI97Au4jUYAktDe3avyA.jpg',
+    genre_ids: [14, 28, 12],
+    id: 464052,
+    media_type: 'movie',
+    original_language: 'en',
+    original_title: 'Wonder Woman 1984',
+    overview:
+      'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s and finds a formidable foe by the name of the Cheetah.',
+    popularity: 2253.729,
+    poster_path: '/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg',
+    release_date: '2020-12-16',
+    title: 'Wonder Woman 1984',
+    video: false,
+    vote_average: 7,
+    vote_count: 3486
+  },
+  {
+    adult: false,
+    backdrop_path: '/sWvxBXNtCOaGdtpKNLiOqmwb10N.jpg',
+    genre_ids: [14, 28],
+    id: 557,
+    media_type: 'movie',
+    original_language: 'en',
+    original_title: 'Spider-Man',
+    overview:
+      'After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers to become the Amazing superhero known as Spider-Man.',
+    popularity: 74.132,
+    poster_path: '/gh4cZbhZxyTbgxQPxD0dOudNPTn.jpg',
+    release_date: '2002-05-01',
+    title: 'Spider-Man',
+    video: false,
+    vote_average: 7.2,
+    vote_count: 13007
+  },
+  {
+    adult: false,
+    backdrop_path: '/9QusGjxcYvfPD1THg6oW3RLeNn7.jpg',
+    genre_ids: [28, 12, 14],
+    id: 297802,
+    media_type: 'movie',
+    original_language: 'en',
+    original_title: 'Aquaman',
+    overview:
+      "Once home to the most advanced civilization on Earth, Atlantis is now an underwater kingdom ruled by the power-hungry King Orm. With a vast army at his disposal, Orm plans to conquer the remaining oceanic people and then the surface world. Standing in his way is Arthur Curry, Orm's half-human, half-Atlantean brother and true heir to the throne.",
+    popularity: 116.623,
+    poster_path: '/5Kg76ldv7VxeX9YlcQXiowHgdX6.jpg',
+    release_date: '2018-12-07',
+    title: 'Aquaman',
+    video: false,
+    vote_average: 6.9,
+    vote_count: 10016
+  },
+  {
+    adult: false,
+    backdrop_path: '/vbY95t58MDArtyUXUIb8Fx1dCry.jpg',
+    genre_ids: [28, 878, 12],
+    id: 1726,
+    media_type: 'movie',
+    original_language: 'en',
+    original_title: 'Iron Man',
+    overview:
+      'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.',
+    popularity: 84.158,
+    poster_path: '/78lPtwv72eTNqFW9COBYI0dWDJa.jpg',
+    release_date: '2008-04-30',
+    title: 'Iron Man',
+    video: false,
+    vote_average: 7.6,
+    vote_count: 19983
+  },
+  {
+    adult: false,
+    backdrop_path: '/w2PMyoyLU22YvrGK3smVM9fW1jj.jpg',
+    genre_ids: [28, 12, 878],
+    id: 299537,
+    media_type: 'movie',
+    original_language: 'en',
+    original_title: 'Captain Marvel',
+    overview:
+      'The story follows Carol Danvers as she becomes one of the universe’s most powerful heroes when Earth is caught in the middle of a galactic war between two alien races. Set in the 1990s, Captain Marvel is an all-new adventure from a previously unseen period in the history of the Marvel Cinematic Universe.',
+    popularity: 222.443,
+    poster_path: '/AtsgWhDnHTq68L0lLsUrCnM7TjG.jpg',
+    release_date: '2019-03-06',
+    title: 'Captain Marvel',
+    video: false,
+    vote_average: 7,
+    vote_count: 10804
+  },
+  {
+    adult: false,
+    backdrop_path: '/kYd1ZMWvG9K2Jeop3XWXcTpJ2dB.jpg',
+    genre_ids: [28, 878],
+    id: 1927,
+    media_type: 'movie',
+    original_language: 'en',
+    original_title: 'Hulk',
+    overview:
+      'Bruce Banner, a genetics researcher with a tragic past, suffers massive radiation exposure in his laboratory that causes him to transform into a raging green monster when he gets angry.',
+    popularity: 44.857,
+    poster_path: '/qnngKqAcqfH2pBxDoKu5lxzSbTo.jpg',
+    release_date: '2003-06-19',
+    title: 'Hulk',
+    video: false,
+    vote_average: 5.5,
+    vote_count: 4182
+  },
+  {
+    adult: false,
+    backdrop_path: '/hkBaDkMWbLaf8B1lsWsKX7Ew3Xq.jpg',
+    genre_ids: [18, 28, 80, 53],
+    id: 155,
+    media_type: 'movie',
+    original_language: 'en',
+    original_title: 'The Dark Knight',
+    overview:
+      'Batman raises the stakes in his war on crime. With the help of Lt. Jim Gordon and District Attorney Harvey Dent, Batman sets out to dismantle the remaining criminal organizations that plague the streets. The partnership proves to be effective, but they soon find themselves prey to a reign of chaos unleashed by a rising criminal mastermind known to the terrified citizens of Gotham as the Joker.',
+    popularity: 73.925,
+    poster_path: '/qJ2tW6WMUDux911r6m7haRef0WH.jpg',
+    release_date: '2008-07-16',
+    title: 'The Dark Knight',
+    video: false,
+    vote_average: 8.5,
+    vote_count: 24416
+  },
+  {
+    adult: false,
+    backdrop_path: '/69EFgWWPFWbRNHmQgYdSnyJ94Ge.jpg',
+    genre_ids: [28, 12, 14, 878],
+    id: 49521,
+    media_type: 'movie',
+    original_language: 'en',
+    original_title: 'Man of Steel',
+    overview:
+      'A young boy learns that he has extraordinary powers and is not of this earth. As a young man, he journeys to discover where he came from and what he was sent here to do. But the hero in him must emerge if he is to save the world from annihilation and become the symbol of hope for all mankind.',
+    popularity: 67.323,
+    poster_path: '/7rIPjn5TUK04O25ZkMyHrGNPgLx.jpg',
+    release_date: '2013-06-12',
+    title: 'Man of Steel',
+    video: false,
+    vote_average: 6.6,
+    vote_count: 11755
+  }
+]
+
+export default movies
