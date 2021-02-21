e56387fb62650562e3c6bdc9db5571b297080627
Chase Terry <chase.terry87@gmail.com>
2021-02-21 01:20:47 -0600

    Moved buttons from SnackList component to Snacks page

--- src/components/SnackList/SnackList.vue
+++ src/components/SnackList/SnackList.vue
@@ -21,18 +21,6 @@
         Your total: ${{ totalSnackPrices }}
       </div>
     </div>
-    <div class="field is-grouped is-grouped-centered mt-4">
-      <div class="control">
-        <router-link to="/seats">
-          <button class="button is-info">Previous</button>
-        </router-link>
-      </div>
-      <div class="control">
-        <router-link to="/payment">
-          <button class="button is-info">Next</button>
-        </router-link>
-      </div>
-    </div>
   </div>
 </template>
 
--- src/views/Snacks.vue
+++ src/views/Snacks.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="container">
     <SnackList />
+    <div class="field is-grouped is-grouped-centered mt-4">
+      <div class="control">
+        <router-link to="/seats">
+          <button data-testid="prev-button" class="button is-info">
+            Previous
+          </button>
+        </router-link>
+      </div>
+      <div class="control">
+        <router-link to="/payment">
+          <button data-testid="next-button" class="button is-info">Next</button>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
