0ed72b17b15e1ee3ad9841d7ddbc7c314d1cf172
Chase Terry <chase.terry87@gmail.com>
2021-03-28 11:44:26 -0500

    Updated footer links

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,6 +1,14 @@
 <template>
   <footer class="footer has-text-light has-text-centered mt-6 is-size-7">
-    &copy; 2020 Muvee Tix
+    <a
+      class="has-text-white is-size-6"
+      href="https://github.com/cdterry87/muvee-tix"
+      target="_blank"
+    >
+      <i class="fab fa-github"></i>
+      View Source on GitHub
+    </a>
+    <p class="mt-4">&copy; 2020 Muvee Tix</p>
   </footer>
 </template>
 
