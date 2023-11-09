955df3bd15364b1c8eef78504acb6c443f7dc10a
Chase Terry <chase.terry87@gmail.com>
2023-11-09 14:58:43 -0600

    Adds publicPath to vue config

--- vue.config.js
+++ vue.config.js
@@ -5,4 +5,5 @@ module.exports = {
       title: 'Muvee Tix - A FAN-tastic place to get your movie tickets!'
     }
   },
+  publicPath: process.env.NODE_ENV === "production" ? "/muvee-tix/" : "/",
 }
