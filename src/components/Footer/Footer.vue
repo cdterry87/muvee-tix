867ee94eb0bf7e8375c4006dc07bcf9ed4b64fa8
Chase Terry <chase.terry87@gmail.com>
2023-11-09 15:29:48 -0600

    Updates TMDB image

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -6,7 +6,10 @@
         href="https://www.themoviedb.org"
         target="_blank"
       >
-        <img src="/tmdb.png" style="height: 64px; width: auto" />
+        <img
+          src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg"
+          style="height: 64px; width: auto"
+        />
       </a>
     </div>
     <div class="mt-2 mb-4 has-text-white">
