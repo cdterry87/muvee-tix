5e915d18125f8240c3cf6888b1a32f7f729b622b
Chase Terry <chase.terry87@gmail.com>
2023-11-09 17:13:06 -0600

    Adds coming soon and updates api call.

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
-import { CORS_ANYWHERE, TVDB_API, TVDB_API_KEY, TVDB_MY_LIST_ID } from './config'
+import { TVDB_API, TVDB_API_KEY, TVDB_MY_LIST_ID } from './config'
 
 export const getMovies = async () => {
   try {
     const response = await axios.get(
-      `${CORS_ANYWHERE}/${TVDB_API}/list/${TVDB_MY_LIST_ID}?api_key=${TVDB_API_KEY}`
+      `${TVDB_API}/list/${TVDB_MY_LIST_ID}?api_key=${TVDB_API_KEY}`
     )
     return response.data.items
   } catch (error) {
--- src/utils/movies.js
+++ src/utils/movies.js
@@ -1,12 +1,14 @@
 export const setMovieDetails = movie => {
   const { id, title, poster_path } = movie
   const link = `/movie/${id}`
+  const comingSoon = require('../assets/comingsoon.jpg')
+
   return {
     id,
     link,
     title,
     image: {
-      src: `https://image.tmdb.org/t/p/w500/${poster_path}`,
+      src: poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : comingSoon,
       alt: `Movie Poster for ${title}`
     }
   }
