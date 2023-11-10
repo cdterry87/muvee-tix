cf494f90e35029ade6b83cf14cbd51713f5b2a85
Chase Terry <chase.terry87@gmail.com>
2023-11-09 23:08:33 -0600

    Updates coming soon and prevents access to movies that aren't out yet.

--- src/utils/movies.js
+++ src/utils/movies.js
@@ -1,7 +1,7 @@
 export const setMovieDetails = movie => {
-  const { id, title, poster_path } = movie
-  const link = `/movie/${id}`
-  const comingSoon = require('../assets/comingsoon.jpg')
+  const { id, title, poster_path, release_date } = movie
+  const link = (release_date && new Date(release_date) <= new Date()) ? `/movie/${id}` : '#'
+  const comingSoon = require('../assets/comingsoon.png')
 
   return {
     id,
